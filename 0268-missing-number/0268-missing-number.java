7dc9b871b68747b530965617c69517eeb9a78457
Fran Pérez <78259026+franpersanchez@users.noreply.github.com>
2022-12-29 12:23:28 +0100

    Time: 6 ms (30.74%), Space: 43 MB (89.06%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for(int i=0; i<n; i++){
+            if(i!=nums[i]){
+                return i;
+            }
+        }
+        return n;
+    }
+}
