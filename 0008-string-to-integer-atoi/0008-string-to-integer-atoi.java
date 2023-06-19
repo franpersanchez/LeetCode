11cfe075e160ce3fb23417959942d16f16201347
Fran Pérez <78259026+franpersanchez@users.noreply.github.com>
2023-06-19 23:40:55 +0200

    Time: 1 ms (100.00%), Space: 41.7 MB (69.63%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,41 +1,56 @@
 class Solution {
     public int myAtoi(String s) {
-        // Base condition
-        if (s == null || s.length() < 1) {
+     //string length
+        final int len = s.length();
+        //if the string have no size or it is null, return 0
+        if (len == 0 || s == null) {
             return 0;
         }
-        // MAX and MIN values for integers
-        final int INT_MAX = 2147483647;
-        final int INT_MIN = -2147483648;
-        // Trimmed string
-        s = s.replaceAll("^\\s+", "");
-        // Counter
-        int i = 0;
-        // Flag to indicate if the number is negative
-        boolean isNegative = s.startsWith("-");
-        // Flag to indicate if the number is positive
-        boolean isPositive = s.startsWith("+");
-        if (isNegative) {
-            i++;
-        } else if (isPositive) {
-            i++;
+        //we start the index at 0
+        int index = 0;
+
+        // skipping all white spaces from the beginning
+        while (index < len && s.charAt(index) == ' ') {
+            ++index;
         }
-        // This will store the converted number
-        double number = 0;
-        // Loop for each numeric character in the string iff numeric characters are leading
-        // characters in the string
-        while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-            number = number * 10 + (s.charAt(i) - '0');
-            i++;
+
+        boolean isNegative = false;
+
+        // to handle sign cases
+        if (index < len) {
+            if (s.charAt(index) == '-') {
+                isNegative = true;
+                ++index;
+            } else if (s.charAt(index) == '+') {
+                ++index;
+            }
         }
-        // Give back the sign to the converted number
-        number = isNegative ? -number : number;
-        if (number < INT_MIN) {
-            return INT_MIN;
+        int result = 0;
+        // converting digit(in character form) to integer form
+        // iterate until non-digit character is not found or we can say iterate till found character is a digit
+        while (index < len && isDigit(s.charAt(index))) {
+
+      /* s.charAt(index) - '0' is to convert the char digit into int digit eg: '5' - '0' --> 5
+      or else it will store the ASCII value of 5 i.e. 53,
+      so we do 53(ASCII of 5) - 48(ASCII of 0(zero)) to get 5 as int*/
+            int digit = s.charAt(index) - '0';
+
+            // to avoid integer overflow
+            if (result > (Integer.MAX_VALUE / 10) || (result == (Integer.MAX_VALUE / 10) && digit > 7)) {
+                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+
+            // adding digits at their desired place-value
+            result = (result * 10) + digit;
+
+            ++index;
         }
-        if (number > INT_MAX) {
-            return INT_MAX;
-        }
-        return (int) number;
+
+        return isNegative ? -result : result;
     }
+
+    private boolean isDigit(char ch) {
+        return ch >= '0' && ch <= '9';
+    }
+
 }
