2d9a3ffde47ae60f253a781ce2030d59ebc7d477
Fran Pérez <78259026+franpersanchez@users.noreply.github.com>
2023-06-20 22:48:38 +0200

    Time: 11 ms (71.03%), Space: 61.3 MB (63.49%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,25 +1,20 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
         int n = nums.length;
-        //we need k numbers at both side of the element
-        int windowSize = 2 * k + 1;
-        //we prefill all elements with -1
         int[] ans = new int[n];
-        Arrays.fill(ans,-1);
+        int windowframe = 2 * k + 1;
+        Arrays.fill(ans, -1);
 
-        if (n < windowSize) {
-            return ans;
-        }
+        if (windowframe > n) return ans;
 
         long[] prefixSum = new long[n + 1];
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             prefixSum[i + 1] = prefixSum[i] + nums[i];
         }
 
-        for (int i = k; i + k < n; ++i) {
-            ans[i] = (int) ((prefixSum[i + k + 1] - prefixSum[i - k]) / windowSize);
+        for (int i = k; i < n - k; i++){
+            ans[i] = (int)(((prefixSum[i+k+1])-prefixSum[i-k])/windowframe);
         }
-
         return ans;
     }
 }
